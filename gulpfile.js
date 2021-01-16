bae618261b0b8f1c381174313f950f7b65013627
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-16 19:31:14 +0000

    File Strucute Changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,60 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
 const cleanCSS = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+const browserify = require('browserify');
+const babel = require('gulp-babel');
+// const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const source = require('vinyl-source-stream');
+const browserSync = require('browser-sync').create();
+
 
 
 function style(){
-    return gulp.src("./scss/**/*.scss")
+    return gulp.src("./src/css/**/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("./css"));
-    }
-    
-    function prefixer(){
-        return gulp.src('./css/style.css')
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(gulp.dest('./css'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.stream());
+    }
+
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
 }
+
+function compile(){
+    let compileOne = gulp.src("./src/js/**/*.js")
+    .pipe(babel({
+        presets: ["@babel/preset-env"]
+      }))
+      .pipe(concat('index.js'))
+      .pipe(gulp.dest('./dist/js'));
+    let compileTwo = browserify('./dist/js/index.js')
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./dist/js'))
+
+    return compileOne, compileTwo;
+}
+
 
 function watchPHP(){
     browserSync.init({
         proxy:{
-            target: "localhost"
+            target: "localhost:8888"
         }
     });
-    gulp.watch("./scss/**/*.scss", gulp.series(style, prefixer))
+    gulp.watch("./src/css/**/*.scss", style);
+    gulp.watch("./src/images/*", images)
     gulp.watch("./**/*.php").on("change", browserSync.reload);
-    gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+    gulp.watch("./src/js/**/*.js", compile).on("change", browserSync.reload);
 }
 
 function watchHTML(){
@@ -39,11 +63,23 @@ function watchHTML(){
             baseDir: "./"
         }
     });
-    gulp.watch("./scss/**/*.scss", gulp.series(style, prefixer))
-    // gulp.watch("./css/**/*.css", prefixer)
+    gulp.watch("./src/css/**/*.scss", style)
+    gulp.watch("./src/images/*", images)
     gulp.watch("./**/*.html").on("change", browserSync.reload);
-    gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+    gulp.watch("./src/js/**/*.js", compile).on("change", browserSync.reload);
 }
 
+
+function gulpExport(){
+    return browserify('./dist/js/index.js')
+    .transform('unassertify', { global: true })
+    .transform('uglifyify', { global: true })
+    .plugin('common-shakeify')
+    .plugin('browser-pack-flat/plugin')
+    .bundle()
+    .pipe(require('minify-stream')({ sourceMap: false }))
+}
+
+exports.gulpExport = gulpExport;
 exports.watchPHP = watchPHP;
 exports.watchHTML = watchHTML;
--- index.php
+++ index.php
@@ -1,67 +1,63 @@
 <?php
 define('PAGE_TITLE', "Home");
-include("./attributes/header.php"); ?>
+include("./dist/attributes/header.php"); ?>
 
 
-<!-- <nav class="nav">
-        <ul class="nav__list">
-            <li><a href="" class="nav__links">About Me</a></li>
-            <li><a href="" class="nav__links">Projects</a></li>
-            <li><a href="" class="nav__links">Contact</a></li>
-        </ul>
-    </nav> -->
+<main>
+    
+    <section class="hero">
+        <div data-scroll class="background__box hero__background"></div>
+        <h1 class="title hero__title">Jack <span class="hero__title__second">Thompson</span></h1>
+        <div class="hero__titleContainer">
+            <h3 class="hero__subtitle subtitle--light">Freelance web Design & Development</h3>
+            <h3 class="hero__subtitle subtitle--bold">Freelance web Design & Development</h3>
+            <h3 class="hero__subtitle subtitle--light">Freelance web Design & Development</h3>
+        </div>
+        <nav class="nav hero__nav">
+            <ul class="nav__list">
+                <li class="nav__links"><a href="#about">> About Me</a></li>
+                <li class="nav__links"><a href="#projects">> Projects</a></li>
+                <li class="nav__links"><a href="">> Contact</a></li>
+            </ul>
+        </nav>
+    </section>
+    
+    <section class="about about__section" id="about">
+        <h2 class="title about__title">About Me</h2>
+        <div class="background__box about__background">
+            <p class="about__info">
+                Hi :) My name is Jack and I'm an IT student with a passion for web design & development, I've been developing websites for over a year both in my spare time and for clients.
+                I'm experienced in HTML, CSS & im currently building my knowledge in PHP & JS to make my websites even better! If you'd like to know more about what i've done in the past you can read my <a href="">full CV</a> too.
+            </p>
+        </div>
+        <img src="./dist/images/me_50.jpg" alt="Long haired guy wearing pink shirt" class="image about__image" loading="lazy">
+        <nav class="nav about__nav">
+            <ul class="nav__list">
+                <li class="nav__links"><a href="#about">> About Me</a></li>
+                <li class="nav__links"><a href="#projects">> Projects</a></li>
+                <li class="nav__links"><a href="">> Contact</a></li>
+            </ul>
+        </nav>
+    </section>
+    
+    <section class="projects projects__section" id="projects">
+    
+        <div class="projects__container">
+            <h2 class="subtitle projects__subtitle">Artist Detox</h2>
+            <img src="./dist/images/artistDetox.png" alt="Laptop showing project website" class="image projects__image" loading="lazy">
+        </div>
+        <div class="projects__container">
+            <h2 class="subtitle projects__subtitle projects__subtitle--sml">Sound Communities</h2>
+            <img src="./dist/images/soundCommunities.png" alt="Computer showing project website" class="image projects__image" loading="lazy">
+        </div>
+        <div class="projects__container">
+            <h2 class="subtitle projects__subtitle">Ocean Youth</h2>
+            <img src="./dist/images/oceanYouth.png" alt="Phone showing project website" class="image projects__image" loading="lazy">
+        </div>
+    </section>
 
-<section class="hero">
-    <div class="background__box hero__background"></div>
-    <h1 class="title hero__title">Jack <span class="hero__title__second">Thompson</span></h1>
-    <div class="hero__titleContainer">
-        <h3 class="hero__subtitle subtitle--light">Freelance web Design & Development</h3>
-        <h3 class="hero__subtitle subtitle--bold">Freelance web Design & Development</h3>
-        <h3 class="hero__subtitle subtitle--light">Freelance web Design & Development</h3>
-    </div>
-    <nav class="nav hero__nav">
-        <ul class="nav__list">
-            <li class="nav__links"><a href="#about">> About Me</a></li>
-            <li class="nav__links"><a href="#projects">> Projects</a></li>
-            <li class="nav__links"><a href="">> Contact</a></li>
-        </ul>
-    </nav>
-</section>
-
-<section class="about about__section" id="about">
-    <h2 class="title about__title">About Me</h2>
-    <div class="background__box about__background">
-        <p class="about__info">
-            Hi :) My name is Jack and I'm an IT student with a passion for web design & development, I've been developing websites for over a year both in my spare time and for clients.
-            I'm experienced in HTML, CSS & im currently building my knowledge in PHP & JS to make my websites even better! If you'd like to know more about what i've done in the past you can read my <a href="">full CV</a> too.
-        </p>
-    </div>
-    <img src="./images/me_50.jpg" alt="Long haired guy wearing pink shirt" class="image about__image" loading="lazy">
-    <nav class="nav about__nav">
-        <ul class="nav__list">
-            <li class="nav__links"><a href="#about">> About Me</a></li>
-            <li class="nav__links"><a href="#projects">> Projects</a></li>
-            <li class="nav__links"><a href="">> Contact</a></li>
-        </ul>
-    </nav>
-</section>
-
-<section class="projects projects__section" id="projects">
-    <h2 class="subtitle projects__title">My Projects</h2>
-
-    <div class="projects__container">
-        <h2 class="subtitle projects__subtitle">Artist Detox</h2>
-        <img src="./images/artistDetox.png" alt="Laptop showing project website" class="image projects__image" loading="lazy">
-    </div>
-    <div class="projects__container">
-        <h2 class="subtitle projects__subtitle projects__subtitle--sml">Sound Communities</h2>
-        <img src="./images/soundCommunities.png" alt="Computer showing project website" class="image projects__image" loading="lazy">
-    </div>
-    <div class="projects__container">
-        <h2 class="subtitle projects__subtitle">Ocean Youth</h2>
-        <img src="./images/oceanYouth.png" alt="Phone showing project website" class="image projects__image" loading="lazy">
-    </div>
-</section>
+    
+</main>
 </body>
 
 
@@ -70,4 +66,4 @@ include("./attributes/header.php"); ?>
 
 
 
-<?php include("./attributes/footer.php");
+<?php include("./dist/attributes/footer.php"); ?>
