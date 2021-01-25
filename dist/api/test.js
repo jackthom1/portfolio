2b6ebf20e29568c75f07507c359de6551c03d43a
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-25 22:02:14 +0000

    module edits

--- dist/api/test.js
+++ dist/api/test.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-    const { name = 'World' } = req.query
-    res.status(200).send(`Hello ${name}!`)
+    const { name = 'World', age = '18' } = req.query
+    res.status(200).send(`Hello ${name}! You're ${age}`)
   }
