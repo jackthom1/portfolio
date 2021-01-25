c9ba76c4c24a2c380d597fb22a83d21ea47480d6
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-25 22:12:20 +0000

    filefinder

--- dist/api/test.js
+++ dist/api/test.js
@@ -1,6 +1,18 @@
+var fs = require('fs');
+
+
+
 module.exports = (req, res) => {
-    const { name = 'World', age = '18' } = req.query
+    // const { name = 'World', age = '18' } = req.query
+    let filename = "../" + req.query + ".html";
+    fs.readFile(filename, function(err, data){
+    if (err){
+        res.writeHead(404, {"Content-Type": "text/html"});
+        return res.end("404 not found");
+    }
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('../index.html')
-    // res.status(200).send(`Hello ${name}! You're ${age}`)
-  }
+    res.write(data);
+    return res.end();
+
+
+        // res.status(200).send(`Hello ${name}! You're ${age}`)
