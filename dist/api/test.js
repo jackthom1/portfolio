c63bcb74f08de4b6eaa6b8e82e94df085d8dc736
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-25 22:18:11 +0000

    rever

--- dist/api/test.js
+++ dist/api/test.js
@@ -1,19 +1,4 @@
-var fs = require('fs');
-
-
-
 module.exports = (req, res) => {
-    // const { name = 'World', age = '18' } = req.query
-    let filename = "../" + req.query + ".html";
-    fs.readFile(filename, function(err, data){
-    if (err){
-        res.writeHead(404, {"Content-Type": "text/html"});
-        return res.end("404 not found");
-    }
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write(data);
-    return res.end();
-    }
-    )}
-
-
+  const { name = 'World', email = '' } = req.query
+  res.status(200).send(`Hello ${name}! Email me on ${email} with your messages`)
+}
