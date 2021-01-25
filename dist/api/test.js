e99832c91bc258ae6938ba837d719719e1bb365f
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-25 22:06:53 +0000

    test

--- dist/api/test.js
+++ dist/api/test.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
     const { name = 'World', age = '18' } = req.query
-    res.status(200).send(`Hello ${name}! You're ${age}`)
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('../index.html')
+    // res.status(200).send(`Hello ${name}! You're ${age}`)
   }
