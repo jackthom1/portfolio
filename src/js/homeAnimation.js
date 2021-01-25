61e26cb42cca5728f274051cb0793bf5500281fb
Jack Thompson <56189597+jackthom1@users.noreply.github.com>
2021-01-25 21:11:25 +0000

    JS Changes

--- src/js/homeAnimation.js
+++ src/js/homeAnimation.js
@@ -1,14 +1,23 @@
 
 const mediaWidth = window.matchMedia("(min-width: 1440px)");
+let pageTitle = document.getElementsByTagName("b")[0].textContent;
 
-if (mediaWidth.matches){
-    const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+console.log(pageTitle)
 
-    tl.fromTo(".hero__background", {scaleY: "0"}, {scaleY: "1", duration: 2});
-    tl.fromTo(".hero__title__second", {x: "100%"}, {x: "0%", duration: 1}, "-=1");
-    tl.fromTo(".hero__title", {x: "-150%"}, {x: "0%", duration: 1}, "-=1");
-    tl.fromTo(".hero__nav", {opacity: "0"}, { opacity: "1", duration: 1}, "-=1");
-    tl.fromTo(".hero__subtitle", {x: "-150%"}, {x: "0%", duration: 1, stagger: "0.15"}, "-=1");
+if (pageTitle == "Home"){
+    if (mediaWidth.matches){
+        const tl = gsap.timeline({defaults: {ease: 'power1.out', duration: 1}});
+        tl.fromTo(".hero__background", {scaleY: "0"}, {scaleY: "1", duration: 2});
+        tl.fromTo(".hero__title", {x: "-150%"}, {x: "0%"}, "-=1");
+        tl.fromTo(".hero__subtitle", {x: "-150%"}, {x: "0%"}, "-=1");
+        tl.fromTo(".hero__nav", {opacity: "0"}, { opacity: "1"}, "-=1");
+    }
+    
+    
+    gsap.to(".projects__title", { scrollTrigger: ".projects__title","left": "100%", duration: .5})
+    gsap.to(".a", {scrollTrigger: ".a", x: 0, opacity: 1, duration: .8})
+    gsap.to(".b", {scrollTrigger: ".b", x: 0, opacity: 1, duration: .8})
+    gsap.to(".c", {scrollTrigger: ".c", x: 0, opacity: 1, duration: .8})
+    gsap.to(".d", {scrollTrigger: ".d", x: 0, opacity: 1, duration: .8})
 }
-
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import barba from '@barba/core';
 
+
+
 let animationEnter = (container) =>{
     // return gsap.from(container, {opacity: 0, duration: 1, clearProps: 'all'})
     const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
@@ -47,43 +49,4 @@ barba.init({
 
 
 
-let items = document.getElementsByClassName("hero__slider")
-let totalItems = items.length
-let slide = 0
 
-items[totalItems - 1].classList.add("hidden");
-items[0].classList.add("shown")
-items[1].classList.add("hidden")
-
-function moveLeft(){
-    gsap.fromTo(items[slide], {x: "0%"}, {x: "-100%", duration: .5})
-    setTimeout(function(){
-        items[slide].classList.add("hidden")
-        items[slide].classList.remove("shown")
-        if (slide === 0){
-            slide = 2
-        }else{
-            slide--
-        }
-        items[slide].classList.remove("hidden");
-        items[slide].classList.add("shown");
-        gsap.fromTo(items[slide], {x: "100%"}, {x: "0%", duration: .5})
-    }, 400)
-    
-    
-}
-function moveRight(){
-    gsap.fromTo(items[slide], {x: "0%"}, {x: "100%", duration: .5})
-    setTimeout(function(){
-        items[slide].classList.add("hidden")
-        items[slide].classList.remove("shown")
-        slide++
-        if (slide === (totalItems)){
-            slide = 0
-        }
-        items[slide].classList.remove("hidden"); 
-        items[slide].classList.add("shown");
-        gsap.fromTo(items[slide], {x: "-100%"}, {x: "0%", duration: .5})
-
-    }, 400)
-}
